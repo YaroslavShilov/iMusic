63bf8213a1d435a58fe4562b34f2bdfae74b2fe2
Yaroslav Shilov <workyarik@gmail.com>
2022-04-04 00:03:54 +0300

    Add token

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,76 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import { Credentials } from "./Credentials";
 import { Aside } from "./common/Aside";
 import { Footer } from "./common/Footer";
 import { Main } from "./common/Main";
+import axios from "axios";
 
 function App() {
-  return (
-    <div>test</div>
+  const spotify = Credentials();
 
-    /*<AppStyle>
-      <Content>
-        <Aside />
-        <Main />
-        <Footer />
-      </Content>
-    </AppStyle>*/
-  );
-}
-
-const Dropdown = () => {
   const data = [
     { value: 1, name: "A" },
     { value: 2, name: "B" },
     { value: 3, name: "C" },
   ];
 
+  const [token, setToken] = useState("");
+  const [genres, setGenres] = useState([]);
+
+  useEffect(() => {
+    axios("https://accounts.spotify.com/api/token", {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization:
+          "Basic " + btoa(spotify.ClientId + ":" + spotify.ClientSecret),
+      },
+      data: "grant_type=client_credentials",
+      method: "POST",
+    }).then((tokenResponse) => {
+      setToken(tokenResponse.data.access_token);
+
+      axios(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+        method: "GET",
+        headers: { Authorization: "Bearer " + tokenResponse.data.access_token },
+      }).then((genreResponse) => {
+        console.log(genreResponse);
+        setGenres(genreResponse.data.categories.items);
+      });
+    });
+  }, []);
+
+  return (
+    <AppStyle>
+      <Content>
+        <Aside />
+        <Main />
+        <Footer />
+      </Content>
+    </AppStyle>
+  );
+}
+
+interface DropdownProps {
+  options: { id: number; name: string }[];
+}
+
+const Dropdown: React.FC<DropdownProps> = ({ options }) => {
+  const [selectedValue, setSelectedValue] = useState<any>(1);
+
   return (
     <div>
-      <select>
-        {data.map((item, index) => (
-          <option key={index} value={item.value}>
+      <select
+        value={selectedValue}
+        onChange={(e) => setSelectedValue(e.target.value)}
+      >
+        {options.map((item, index) => (
+          <option key={index} value={item.id}>
             {item.name}
           </option>
         ))}
       </select>
+      <p>{selectedValue}</p>
     </div>
   );
 };
---
+++ src/Credentials.js
@@ -0,0 +1,6 @@
+export const Credentials = () => {
+  return {
+    ClientId: "db4b1563607f44dcb6828aefd1dae9cf",
+    ClientSecret: "ab1da000c8274104b6c1d1599463c554",
+  };
+};
